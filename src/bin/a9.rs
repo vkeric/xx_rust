95794b69e6d5500b24800111ab4f2f925ac33028
鸵鸟 <2950995347@qq.com>
2024-06-14 22:49:54 +0800

    元组

---
+++ src/bin/a9.rs
@@ -0,0 +1,16 @@
+
+
+fn coordinate()->(i32,i32){
+    (1,7)
+}
+
+fn main(){
+    let (x, y) = coordinate();
+    if y>5{
+        println!(">5");
+    }else if y<5{
+        println!("<5");
+    } else{
+        println!("=5")
+    }
+}
