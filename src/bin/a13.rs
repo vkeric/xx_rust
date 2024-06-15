916bab300e100a8d70c75d2572e72399b7b3ccea
鸵鸟 <2950995347@qq.com>
2024-06-15 22:16:57 +0800

    a13

--- src/bin/a13.rs
+++ src/bin/a13.rs
@@ -6,13 +6,13 @@
 
 
 fn main(){
+    let my_numbers = vec![10,20,30,40];
 
-    let my_number = vec![10,20,30,40];
-    for num in &my_number{
+    for num in &my_numbers {
         match num {
-            30=>println!("30"),
-            _=>println!("{:?},"num),
+            30 =>println!("30"),
+            _ =>println!("{:?}",anum),
         }
     }
-    println("{:?}", my_number.len());
+    println!("{:?}", my_numbers.len());
 }
