216ed84a7dee260a87b8c71e17e7d6f613c48c37
鸵鸟 <2950995347@qq.com>
2024-06-15 09:02:03 +0800

    ~

--- src/bin/a10.rs
+++ src/bin/a10.rs
@@ -1,1 +1,13 @@
-// 所有权
+fn print_message(gt_100:bool){
+    match gt_100{
+        true=>println!("big"),
+        false=>println!("small"),
+    }
+}
+
+
+fn main(){
+    let value=100;
+    let is_gt_100 = value>100;
+    print_message(is_gt_100);
+}
