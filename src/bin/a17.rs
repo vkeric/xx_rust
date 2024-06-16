d7853a452ad96d992a40d32c88986fcc7f3f1146
鸵鸟 <2950995347@qq.com>
2024-06-16 15:59:13 +0800

    a17

---
+++ src/bin/a17.rs
@@ -0,0 +1,10 @@
+// 关于浏览器标准文档
+
+
+fn main(){
+    let my_str = "thsi is my STRING";
+    println!("{:?}", my_str.to_uppercase());
+
+    // 将字符串转换为小写，并打印
+    println!("{:?}", my_str.to_lowercase());
+}
